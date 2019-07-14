42e0f32413282308598f5e2539978e28242b1b1a
Brittany Hancock <bhancock@utah.gov>
2019-07-14 15:44:02 -0600

    Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SuperSecret from './components/SuperSecret';
+import OnOffButton from './components/OnOffButton';
+import AccordianMenu from './components/AccordianMenu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SuperSecret isAuthenticated={true}/>
+        <OnOffButton/>
+        <AccordianMenu>
+          This is the Accordian Menu
+        </AccordianMenu>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/hocs/withToggle.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default function withToggle(WrappedComponent){
+    return class WithToggle extends Component{
+        state = {
+            toggle: false
+        };
+        handleChange = e => {
+            this.setState({
+                toggle: !this.state.toggle
+            })
+        };
+        render(){
+            let toggle = {
+                value: this.state.toggle,
+                handleChange: this.handleChange
+            }
+            return <WrappedComponent toggle={toggle} {...this.props}/>
+        }
+    }
+    
+}
